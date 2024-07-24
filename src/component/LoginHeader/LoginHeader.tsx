1bc5b1c0b80b1e742155be91c48c62232422855a
1998kapil <kapil.pawar@escalesolutions.com>
2024-07-24 17:57:01 +0530

    Loged in user layout structure done

---
+++ src/component/LoginHeader/LoginHeader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginHeader = () => {
+    return (
+        <div className='h-16'>
+            LoginHeader
+        </div>
+    )
+}
+
+export default LoginHeader
---
+++ src/component/Sidebar/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='h-full'>
+        SideBar <br/>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa at labore debitis minus deserunt architecto autem! Quas eligendi, libero ratione nulla incidunt illo nam blanditiis similique, esse cumque iure cupiditate?lorem200</div>
+  )
+}
+
+export default Sidebar
